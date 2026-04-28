69ee0606f50a3bf11256ec81d93fe70f2c0cb8d0
Pawat001 <s6506022620109@email.kmutnb.ac.th>
2026-04-28 12:01:46 +0700

    save

---
+++ app/Low_levelspark.py
@@ -0,0 +1,16 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import IntegerType
+from pyspark.sql.functions import count, countDistinct, first, last, min, max, sum, sumDistinct, avg
+
+# 1. Initialize Spark Session
+# ใช้ Builder pattern ในการสร้าง Session
+spark = SparkSession.builder \
+    .appName("CleanOutput") \
+    .getOrCreate()
+
+# 2. Configure Logging
+# ลดความวุ่นวายของ Log ให้เหลือแค่ Error เท่านั้น
+spark.sparkContext.setLogLevel("ERROR")
+
+read_file = spark.read.format("csv").option("header", True).load("fb_live_thailand.csv")  # Read file
+read_file.printSchema()  # Print schema
